8d9c9d3c0d3e821a661221753bff9e3b2be45c20
vinicius barreto barbosa <vinicius.vbb98@gmail.com>
2021-08-25 11:20:43 -0300

    metodos novos da classe postagem

--- src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,8 @@
 package br.org.generation.blogpessoal.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import br.org.generation.blogpessoal.model.Postagem;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem,Long> {
-
+		
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 }
